b83621a4de615777455ab5ba178c0ac16106eaa4
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2023-04-28 19:02:38 +0200

    Terminer les navbae et la footer

---
+++ resources/views/Layouts/left_navbar.blade.php
@@ -0,0 +1,21 @@
+<div class="sidebar-inner slimscroll">
+    <div id="sidebar-menu" class="sidebar-menu">
+        <ul>
+            <li class="menu-title">
+                <span>Menu Principal</span>
+            </li>
+            <li>
+                <a href="{{url('/dashboard')}}">
+                    <i class="feather-grid"></i>
+                    <span> Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="feather-user"></i>
+                    <span> Profil</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,17 @@
         <div class="main-wrapper">
             <div class="header">
                 @include("Layouts.top_navbar")
-
             </div>
+            <div class="sidebar" id="sidebar">
+                @include("Layouts.left_navbar")
+            </div>
+            <div class="page-wrapper">
+                <div class="content container-fluid">
+               
+                </div>
+                
+            </div>
+            @include("Layouts.footer")
         </div>
         @include("Layouts.script_site")
     </body>
