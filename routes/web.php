a0d1b244a864a79624b0484505730535fdf3acbd
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2023-04-28 10:42:09 +0200

    Importer les fichier de template login et créer le controller authentification

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthentificationController;
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::controller(AuthentificationController::class)->group(function() {
+        
+    });
+?>
